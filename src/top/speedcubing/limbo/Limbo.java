816293d4ce5104b4a599b2fab55a0d550f20d17d
speedcubing <speedcubingthebest@gmail.com>
2024-04-17 19:18:10 +0800

    [perf] perf

--- src/top/speedcubing/limbo/Limbo.java
+++ src/top/speedcubing/limbo/Limbo.java
@@ -1,46 +1,26 @@
 package top.speedcubing.limbo;
 
 import net.minecraft.server.v1_8_R3.MinecraftServer;
-import org.bukkit.*;
+import org.bukkit.Bukkit;
+import org.bukkit.Difficulty;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
-import org.bukkit.event.*;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.entity.*;
-import org.bukkit.event.player.*;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import top.speedcubing.lib.bukkit.pluginMessage.BungeePluginMessage;
-
-import java.net.*;
-import java.nio.file.*;
+import top.speedcubing.server.utils.MapManager;
 
 public class Limbo extends JavaPlugin implements Listener {
-    //MapManager
-    public static World install(String url, String map, Difficulty difficulty, int time, boolean autoSave) throws Exception {
-        long t = System.currentTimeMillis();
-        World world = Bukkit.getWorld(map);
-        System.out.println("[MapManager] installing \"" + map + "\"" + (url == null ? "" : (" from \"" + map + "\"")));
-        if (url != null) {
-            new ProcessBuilder("rm", "-r", map).start().waitFor();
-            String file = url + ".tar.gz";
-
-            HttpURLConnection connection = (HttpURLConnection) new URL("https://speedcubing.top/maps/" + file).openConnection();
-            connection.addRequestProperty("User-Agent", "Mozilla/4.0");
-            String key = "2ad5a999-854a-4c5e-adbc-aa434deb79d3";
-            connection.setRequestProperty("Authorization", "Bearer " + key);
-
-            Files.copy(connection.getInputStream(), Paths.get(file), StandardCopyOption.REPLACE_EXISTING);
-            new ProcessBuilder("tar", "-xvzf", file).start().waitFor();
-            new ProcessBuilder("mv", url, map).start().waitFor();
-            new ProcessBuilder("rm", file).start().waitFor();
-            world = Bukkit.createWorld(new WorldCreator(map));
-        }
-        world.setDifficulty(difficulty);
-        world.setTime(time);
-        world.setAutoSave(autoSave);
-        t = System.currentTimeMillis() - t;
-        System.out.println("[MapManager] \"" + map + "\" installing finished. (" + String.format("%.3fs", t / 1000D) + ")");
-        return world;
-    }
 
     public void lobby(Player player) {
         BungeePluginMessage.switchServer(player, "lobby");
@@ -50,15 +30,8 @@ public class Limbo extends JavaPlugin implements Listener {
         MinecraftServer.getServer().getPropertyManager().setProperty("server-name", "limbo");
         Bukkit.getPluginManager().registerEvents(this, this);
 
-        Bukkit.getPluginCommand("hub").setExecutor((commandSender, b, c, strings) -> {
-            if (strings.length == 0) {
-                lobby((Player) commandSender);
-            } else commandSender.sendMessage("/hub, /l, /lobby");
-            return true;
-        });
-
         try {
-            install("map_limbo", "map_limbo", Difficulty.PEACEFUL, 6000, false);
+            MapManager.install("map_limbo", "map_limbo", Difficulty.PEACEFUL, 6000, false);
         } catch (Exception e) {
             e.printStackTrace();
             Bukkit.getServer().shutdown();
